a41636da9d1018db7c0fb29caf402861fc7c6b47
Breno Angelo <brenoangeloal@gmail.com>
2021-10-24 15:28:51 -0300

    consuming from the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
+interface IWeatherMain {
+  temp: number;
+  feels_like: number;
+  temp_max: number;
+  temp_min: number;
+  humidity: number;
+  pressure: number;
+}
+
+interface IWeather {
+  name: string;
+  main: IWeatherMain;
+  weather: [
+    {
+      main: string;
+      description: string;
+    }
+  ]
+}
 
 function App() {
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState<IWeather>();
+
+  async function getWeather(lat: Number, long: Number) {
+    const res = await axios.get<IWeather>('http://api.openweathermap.org/data/2.5/weather', {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WEATHER_KEY,
+        lang: 'pt',
+        units: 'metric'
+      }
+    })
+
+    console.log(res.data)
+    setWeather(res.data);
+    
+  }
+
+  useEffect(() =>{
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude)
+      setLocation(true)
+    })
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {
+          location ? (
+            <>
+              <h3>Clima nas suas coordenadas</h3>
+              <ul>
+                <li>Local: {weather?.name}</li>
+                <li>Temperatura atual: {weather?.main.temp}</li>
+                <li>Sensação termica: {weather?.main.feels_like}</li>
+                <li>Temperatura máxima: {weather?.main.temp_max}</li>
+                <li>Temperatura minima: {weather?.main.temp_min}</li>
+                <li>Pressão: {weather?.main.pressure} hpa</li>
+                <li>Umidade: {weather?.main.humidity}%</li>
+                <li>{weather?.weather[0].description}</li>
+              </ul>
+            </>
+          ) : (
+            <>
+              Você precisa habilitar a localização no browser
+            </>
+          )
+        }
     </div>
   );
 }
